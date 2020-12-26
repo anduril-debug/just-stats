c63bdd9d6605bbbf08624c7270b31b5cb8278bfa
Luka Lortkipanidze <lukalortk16@gmail.com>
2020-12-26 21:20:00 +0400

    added error handling

--- update.py
+++ update.py
@@ -2,4 +2,8 @@ from get_stats import main
 
 link = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures"
 
-main(link)
+
+try:
+	main(link)
+except:
+	print("Something went wrong.....")
