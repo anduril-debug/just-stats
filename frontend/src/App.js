0249d3da5c7ebffefe7b7084206d2370f86a5d2d
Luka Lortkipanidze <lukalortk16@gmail.com>
2021-01-16 00:28:24 +0400

    added seperated frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar />
+
+      <div className="container">
+      </div>
+    </div>
+  );
+}
+
+export default App;
