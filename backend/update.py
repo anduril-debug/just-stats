6c3d30445ed24c6e52b45c9393b272f23b5d5725
unknown <lukalortk16@gmail.com>
2021-02-01 18:43:08 +0400

    update

--- backend/update.py
+++ backend/update.py
@@ -1,5 +1,4 @@
 from get_stats import main, upcoming_games
-from just_stats.models import Match,Team,Player, Match_Player_Stats
 
 
 link = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures"
