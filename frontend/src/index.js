77b525976d9034563311f69da8c7109df484f347
unknown <lukalortk16@gmail.com>
2021-02-02 21:46:11 +0400

    commented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { createStore } from 'redux';
-// import allReducers from './reducers';
 import { Provider } from 'react-redux';
 import store from './store';
 
-// const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 ReactDOM.render(
