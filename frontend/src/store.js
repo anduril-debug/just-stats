eb7c825ed80fcd294a5714e85ece9a0f012e0dac
unknown <lukalortk16@gmail.com>
2021-02-02 21:47:27 +0400

    '_'

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import counter from './redux/counter/counterSlice';
 import teams from './redux/teams/teamsSlice';
 import matches from './redux/matches/matchesSlice';
 
+
 export default configureStore({
     reducer: {
         counter,
